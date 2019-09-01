54de03b2ffcdcd02b907e146febb4c2af04e46f6
mylove90 <engm.saad1990@gmail.com>
2019-09-02 00:34:09 +0200

    Split Build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Android Kernel Build Script
 
 # Main environtment
-KERNEL_DIR=${HOME}/$(basename $(pwd))
+KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 ZIP_DIR=$KERNEL_DIR/AnyKernel3
 CONFIG=onclite-perf_defconfig
@@ -17,18 +17,6 @@ PATH="${KERNEL_DIR}/stock/bin:${PATH}:${KERNEL_DIR}/stock_32/bin:${PATH}"
 export ARCH=arm64
 export CROSS_COMPILE
 export CROSS_COMPILE_ARM32
-
-# Install build package for debian based linux
-sudo apt install bc bash git-core gnupg build-essential \
-    zip curl make automake autogen autoconf autotools-dev libtool shtool python \
-    m4 gcc libtool zlib1g-dev flex
-
-# Clone toolchain
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r45 --depth=1 stock
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r45 --depth=1 stock_32
-
-# Clone AnyKernel3
-git clone https://github.com/rama982/AnyKernel3 -b onc-miui
 
 # Build start
 make O=out $CONFIG
