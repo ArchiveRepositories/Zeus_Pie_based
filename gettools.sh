5609322433f086a45928d1667d9f5ad28c04a188
mylove90 <engm.saad1990@gmail.com>
2019-10-30 07:19:09 +0000

    Update GCC toolchain

--- gettools.sh
+++ gettools.sh
@@ -10,8 +10,8 @@ sudo apt install bc bash git-core gnupg build-essential \
     m4 gcc libtool zlib1g-dev flex bison libssl-dev
 
 # Clone toolchain
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r45 --depth=1 stock
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r45 --depth=1 stock_32
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r49 --depth=1 stock
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r49 --depth=1 stock_32
 
 # Clone AnyKernel3
 git clone https://github.com/rama982/AnyKernel3 -b onc-miui
