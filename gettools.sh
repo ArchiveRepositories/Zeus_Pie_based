2cfe9e2f81b134268767c02ea834c888d1c46ff0
ragy <wad.moskla@gmail.com>
2019-11-29 03:40:28 +0200

    Added FP Boost Added Qnovo5 Driver Enabled Force Fast Charge By Default Updated Clang & Toolchain

--- gettools.sh
+++ gettools.sh
@@ -10,14 +10,11 @@ sudo apt install bc bash git-core gnupg build-essential \
     m4 gcc libtool zlib1g-dev flex bison libssl-dev
 
 # Clone toolchain
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r49 --depth=1 stock
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r49 --depth=1 stock_32
-
-# Clone AnyKernel3
-git clone https://github.com/rama982/AnyKernel3 -b onc-miui
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r50 --depth=1 stock
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r50 --depth=1 stock_32
 
 #Download Clang
-wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/android-9.0.0_r6/clang-4691093.tar.gz
+wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/android-9.0.0_r50/clang-4691093.tar.gz
 mkdir -p clang/clang-4691093/
 tar xvzf clang-4691093.tar.gz -C clang/clang-4691093
 rm clang-4691093.tar.gz
